336b3f6a01533d761d81213b63632b3848176a37
Florent Nosari <nosari1u@fst-ats-315-l07.fst.site.univ-lorraine.fr>
2017-03-15 09:13:59 +0100

    Lesson 1 : second attempt - draw line (better)

--- common/drawer.cpp
+++ common/drawer.cpp
@@ -5,9 +5,9 @@ Drawer::Drawer()
 }
 
 void Drawer::line(const int x0, const int y0, const int x1, const int y1, TGAImage &image, TGAColor color){
-    for (float t=0.; t<1.; t+=.01) {
-        int x = x0*(1.-t) + x1*t;
-        int y = y0*(1.-t) + y1*t;
-        image.set(x, y, color);
-    }
+    for (int x=x0; x<=x1; x++) {
+            float t = (x-x0)/(float)(x1-x0);
+            int y = y0*(1.-t) + y1*t;
+            image.set(x, y, color);
+        }
 }
--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 
 int main(){
 
     TGAImage image(100, 100, TGAImage::RGB);
     Drawer::line(13, 20, 80, 40, image, white);
+    Drawer::line(20, 13, 40, 80, image, red);
+    Drawer::line(0, 0, 53, 40, image, blue);
     image.flip_vertically();
     image.write_tga_file("output.tga");
 
